818a8b73c61eaf0963e70c73d6ee722aea59f6a1
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-02-20 04:20:30 +0700

    Move codes to Core

--- Scripts/GameData/BaseStage.cs
+++ Scripts/GameData/BaseStage.cs
@@ -2,35 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[System.Serializable]
-public class StageFoe
-{
-    public CharacterItem character;
-    public int level;
-}
-
-[System.Serializable]
-public class StageRandomFoe
-{
-    public StageFoe[] foes;
-    public int randomWeight;
-}
-
-[System.Serializable]
-public class StageWave
-{
-    public bool useRandomFoes;
-    public StageFoe[] foes;
-}
-
-public class Stage : BaseGameData
+public class BaseStage : BaseGameData
 {
     public string stageNumber;
     public Sprite icon;
-    public EnvironmentData environment;
-    [Header("Battle")]
-    public StageWave[] waves;
-    public StageRandomFoe[] randomFoes;
     [Header("Stamina")]
     public int requireStamina;
     [Header("Rewards")]
@@ -41,16 +16,6 @@ public class Stage : BaseGameData
     public ItemDrop[] rewardItems;
     [Header("Unlock")]
     public Stage[] unlockStages;
-
-    public StageRandomFoe RandomFoes()
-    {
-        var weight = new Dictionary<StageRandomFoe, int>();
-        foreach (var randomFoe in randomFoes)
-        {
-            weight.Add(randomFoe, randomFoe.randomWeight);
-        }
-        return WeightedRandomizer.From(weight).TakeOne();
-    }
 
     public virtual string ToJson()
     {
