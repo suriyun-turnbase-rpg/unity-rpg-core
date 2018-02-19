d7e822be626badd22b3f3c0328e9eda4e2da38e5
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2018-02-20 06:06:45 +0700

    Remanage Core codes

---
+++ Scripts/GamePlay/BaseDamage.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(TargetingRigidbody))]
+public abstract class BaseDamage : MonoBehaviour
+{
+    private Transform tempTransform;
+    public Transform TempTransform
+    {
+        get
+        {
+            if (tempTransform == null)
+                tempTransform = GetComponent<Transform>();
+            return tempTransform;
+        }
+    }
+    private Rigidbody tempRigidbody;
+    public Rigidbody TempRigidbody
+    {
+        get
+        {
+            if (tempRigidbody == null)
+                tempRigidbody = GetComponent<Rigidbody>();
+            return tempRigidbody;
+        }
+    }
+    private TargetingRigidbody tempTargetingRigidbody;
+    public TargetingRigidbody TempTargetingRigidbody
+    {
+        get
+        {
+            if (tempTargetingRigidbody == null)
+                tempTargetingRigidbody = GetComponent<TargetingRigidbody>();
+            return tempTargetingRigidbody;
+        }
+    }
+}
