5fef877b9ac31f9e2d42f0047bafa02d10d8def6
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-12-12 18:27:44 +0700

    Remove unused codes

--- Scripts/GameData/Animation/BaseAttackAnimationData.cs
+++ Scripts/GameData/Animation/BaseAttackAnimationData.cs
@@ -7,23 +7,9 @@ using UnityEditor;
 
 public abstract class BaseAttackAnimationData : AnimationData
 {
-    [System.Obsolete("We're going to use `hitDurationRate`, you can use context menu `Set Hit Duration Rate` to set it, This will be removed on next version")]
-    public float hitDuration = 0;
     [Range(0f, 1f)]
     public float hitDurationRate;
     public BaseDamage damage;
-
-    [ContextMenu("Set Hit Duration Rate")]
-    public void SetHitDurationRate()
-    {
-        if (hitDuration > 0 && AnimationDuration > 0)
-            hitDurationRate = hitDuration / AnimationDuration;
-        if (hitDurationRate > 1)
-            hitDurationRate = 1;
-#if UNITY_EDITOR
-        EditorUtility.SetDirty(this);
-#endif
-    }
 }
 
 public static class BaseAttackAnimationDataExtension
