552a1706c970eb6604f2ca48843d506496b80e9d
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2020-01-25 16:04:55 +0700

    WIP hard currency convertion

---
+++ Scripts/GameData/HardCurrencyConvertion.cs
@@ -0,0 +1,12 @@
+﻿[System.Serializable]
+public struct HardCurrencyConvertion
+{
+    public int requireHardCurrency;
+    public int receiveSoftCurrency;
+
+    public string ToJson()
+    {
+        return "{\"requireHardCurrency\":" + requireHardCurrency + "," +
+            "\"receiveSoftCurrency\":" + receiveSoftCurrency + "}";
+    }
+}
