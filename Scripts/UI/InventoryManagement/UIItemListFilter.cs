be393d575f004a9d8cb8659a1579725e6dec38f6
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2020-03-05 05:18:34 +0700

    Add material to item list filter and also fix bugs

--- Scripts/UI/InventoryManagement/UIItemListFilter.cs
+++ Scripts/UI/InventoryManagement/UIItemListFilter.cs
@@ -3,6 +3,7 @@ public struct UIItemListFilterSetting
 {
     public bool showCharacter;
     public bool showEquipment;
+    public bool showMaterial;
     public string category;
 }
 
@@ -10,29 +11,37 @@ public static class UIItemListFilter
 {
     public static bool Filter(PlayerItem item, UIItemListFilterSetting setting)
     {
-        var isCharacter = IsCharacter(item);
-        var isEquipment = IsEquipment(item);
-        if (setting.showCharacter && !isCharacter)
-            return false;
-        if (!setting.showCharacter && isCharacter)
-            return false;
-        if (setting.showEquipment && !isEquipment)
-            return false;
-        if (!setting.showEquipment && isEquipment)
-            return false;
-        if (!string.IsNullOrEmpty(setting.category) && !MatchCategory(item, setting.category))
-            return false;
-        return true;
+        if (!setting.showCharacter &&
+            !setting.showEquipment &&
+            !setting.showMaterial &&
+            string.IsNullOrEmpty(setting.category))
+        {
+            // No setting, No filter, Show all
+            return true;
+        }
+        if ((setting.showMaterial && IsMaterial(item)) ||
+            (setting.showCharacter && IsCharacter(item)) ||
+            (setting.showEquipment && IsEquipment(item)))
+        {
+            // Filter by item types and category
+            return string.IsNullOrEmpty(setting.category) || MatchCategory(item, setting.category);
+        }
+        return false;
     }
 
     public static bool IsCharacter(PlayerItem item)
     {
-        return item != null && item.ItemData != null && item.CharacterData != null;
+        return item != null && item.CharacterData != null;
     }
 
     public static bool IsEquipment(PlayerItem item)
     {
-        return item != null && item.ItemData != null && item.EquipmentData != null;
+        return item != null && item.EquipmentData != null;
+    }
+
+    public static bool IsMaterial(PlayerItem item)
+    {
+        return item != null && item.MaterialData != null && !IsCharacter(item) && !IsEquipment(item);
     }
 
     public static bool MatchCategory(PlayerItem item, string category)
