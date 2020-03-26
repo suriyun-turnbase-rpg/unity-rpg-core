96120c5603a4200a6a763ef29b1f8d5740d1efcd
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2020-03-27 05:19:51 +0700

    Add clan function and events to UIPlayer

--- Scripts/UI/Player/UIPlayer.cs
+++ Scripts/UI/Player/UIPlayer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
+using UnityEngine.Serialization;
 
 public class UIPlayer : UIDataItem<Player>
 {
@@ -10,14 +11,24 @@ public class UIPlayer : UIDataItem<Player>
     public UILevel uiLevel;
     public UIItem uiMainCharacter;
     public UIPlayerList uiPlayerList;
+    [Header("Friend buttons")]
     public Button buttonFriendRequest;
     public Button buttonFriendAccept;
     public Button buttonFriendDecline;
     public Button buttonFriendDelete;
-    public Button buttonRequestDelete;
+    [FormerlySerializedAs("buttonRequestDelete")]
+    public Button buttonFriendRequestDelete;
+    [Header("Clan buttons")]
+    public Button buttonClanJoinAccept;
+    public Button buttonClanJoinDecline;
+    public Button buttonClanMemberDelete;
+    public Button buttonClanOwnerTransfer;
+    public Button buttonClanJoinRequestDelete;
+    [Header("Arena UIs")]
     public Text textArenaScore;
     public UIArenaRank uiArenaRank;
     // Events
+    [Header("Friend events")]
     public UnityEvent eventFriendRequestSuccess;
     public UnityEvent eventFriendRequestFail;
     public UnityEvent eventFriendAcceptSuccess;
@@ -28,10 +39,22 @@ public class UIPlayer : UIDataItem<Player>
     public UnityEvent eventFriendDeleteFail;
     public UnityEvent eventFriendRequestDeleteSuccess;
     public UnityEvent eventFriendRequestDeleteFail;
+    [Header("Clan events")]
+    public UnityEvent eventClanJoinAcceptSuccess;
+    public UnityEvent eventClanJoinAcceptFail;
+    public UnityEvent eventClanJoinDeclineSuccess;
+    public UnityEvent eventClanJoinDeclineFail;
+    public UnityEvent eventClanMemberDeleteSuccess;
+    public UnityEvent eventClanMemberDeleteFail;
+    public UnityEvent eventClanOwnerTransferSuccess;
+    public UnityEvent eventClanOwnerTransferFail;
+    public UnityEvent eventClanJoinRequestDeleteSuccess;
+    public UnityEvent eventClanJoinRequestDeleteFail;
 
     public override void UpdateData()
     {
         SetupInfo(data);
+        // Friend buttons
         if (buttonFriendRequest != null)
         {
             buttonFriendRequest.onClick.RemoveListener(OnClickFriendRequest);
@@ -56,11 +79,42 @@ public class UIPlayer : UIDataItem<Player>
             buttonFriendDelete.onClick.AddListener(OnClickFriendDelete);
             buttonFriendDelete.gameObject.SetActive(!IsEmpty());
         }
-        if (buttonRequestDelete != null)
+        if (buttonFriendRequestDelete != null)
         {
-            buttonRequestDelete.onClick.RemoveListener(OnClickRequestDelete);
-            buttonRequestDelete.onClick.AddListener(OnClickRequestDelete);
-            buttonRequestDelete.gameObject.SetActive(!IsEmpty());
+            buttonFriendRequestDelete.onClick.RemoveListener(OnClickRequestDelete);
+            buttonFriendRequestDelete.onClick.AddListener(OnClickRequestDelete);
+            buttonFriendRequestDelete.gameObject.SetActive(!IsEmpty());
+        }
+        // Clan buttons
+        if (buttonClanJoinAccept != null)
+        {
+            buttonClanJoinAccept.onClick.RemoveListener(OnClickClanJoinAccept);
+            buttonClanJoinAccept.onClick.AddListener(OnClickClanJoinAccept);
+            buttonClanJoinAccept.gameObject.SetActive(!IsEmpty());
+        }
+        if (buttonClanJoinDecline != null)
+        {
+            buttonClanJoinDecline.onClick.RemoveListener(OnClickClanJoinDecline);
+            buttonClanJoinDecline.onClick.AddListener(OnClickClanJoinDecline);
+            buttonClanJoinDecline.gameObject.SetActive(!IsEmpty());
+        }
+        if (buttonClanMemberDelete != null)
+        {
+            buttonClanMemberDelete.onClick.RemoveListener(OnClickClanMemberDelete);
+            buttonClanMemberDelete.onClick.AddListener(OnClickClanMemberDelete);
+            buttonClanMemberDelete.gameObject.SetActive(!IsEmpty());
+        }
+        if (buttonClanOwnerTransfer != null)
+        {
+            buttonClanOwnerTransfer.onClick.RemoveListener(OnClickClanOwnerTransfer);
+            buttonClanOwnerTransfer.onClick.AddListener(OnClickClanOwnerTransfer);
+            buttonClanOwnerTransfer.gameObject.SetActive(!IsEmpty());
+        }
+        if (buttonClanJoinRequestDelete != null)
+        {
+            buttonClanJoinRequestDelete.onClick.RemoveListener(OnClickClanJoinRequestDelete);
+            buttonClanJoinRequestDelete.onClick.AddListener(OnClickClanJoinRequestDelete);
+            buttonClanJoinRequestDelete.gameObject.SetActive(!IsEmpty());
         }
     }
 
@@ -210,5 +264,102 @@ public class UIPlayer : UIDataItem<Player>
         GameInstance.Singleton.OnGameServiceError(error);
         if (eventFriendRequestDeleteFail != null)
             eventFriendRequestDeleteFail.Invoke();
+    }
+
+    public void OnClickClanJoinAccept()
+    {
+        GameInstance.GameService.ClanJoinAccept(data.Id, ClanJoinAcceptSuccess, ClanJoinAcceptFail);
+    }
+
+    private void ClanJoinAcceptSuccess(GameServiceResult result)
+    {
+        if (uiPlayerList != null)
+            uiPlayerList.RemoveListItem(data.Id);
+        if (eventClanJoinAcceptSuccess != null)
+            eventClanJoinAcceptSuccess.Invoke();
+    }
+
+    private void ClanJoinAcceptFail(string error)
+    {
+        GameInstance.Singleton.OnGameServiceError(error);
+        if (eventClanJoinAcceptFail != null)
+            eventClanJoinAcceptFail.Invoke();
+    }
+    public void OnClickClanJoinDecline()
+    {
+        GameInstance.GameService.ClanJoinDecline(data.Id, ClanJoinDeclineSuccess, ClanJoinDeclineFail);
+    }
+
+    private void ClanJoinDeclineSuccess(GameServiceResult result)
+    {
+        if (uiPlayerList != null)
+            uiPlayerList.RemoveListItem(data.Id);
+        if (eventClanJoinDeclineSuccess != null)
+            eventClanJoinDeclineSuccess.Invoke();
+    }
+
+    private void ClanJoinDeclineFail(string error)
+    {
+        GameInstance.Singleton.OnGameServiceError(error);
+        if (eventClanJoinDeclineFail != null)
+            eventClanJoinDeclineFail.Invoke();
+    }
+
+    public void OnClickClanMemberDelete()
+    {
+        GameInstance.GameService.ClanMemberDelete(data.Id, ClanMemberDeleteSuccess, ClanMemberDeleteFail);
+    }
+
+    private void ClanMemberDeleteSuccess(GameServiceResult result)
+    {
+        if (uiPlayerList != null)
+            uiPlayerList.RemoveListItem(data.Id);
+        if (eventClanMemberDeleteSuccess != null)
+            eventClanMemberDeleteSuccess.Invoke();
+    }
+
+    private void ClanMemberDeleteFail(string error)
+    {
+        GameInstance.Singleton.OnGameServiceError(error);
+        if (eventClanMemberDeleteFail != null)
+            eventClanMemberDeleteFail.Invoke();
+    }
+
+    public void OnClickClanOwnerTransfer()
+    {
+        GameInstance.GameService.ClanOwnerTransfer(data.Id, ClanOwnerTransferSuccess, ClanOwnerTransferFail);
+    }
+
+    private void ClanOwnerTransferSuccess(GameServiceResult result)
+    {
+        if (eventClanOwnerTransferSuccess != null)
+            eventClanOwnerTransferSuccess.Invoke();
+    }
+
+    private void ClanOwnerTransferFail(string error)
+    {
+        GameInstance.Singleton.OnGameServiceError(error);
+        if (eventClanOwnerTransferFail != null)
+            eventClanOwnerTransferFail.Invoke();
+    }
+
+    public void OnClickClanJoinRequestDelete()
+    {
+        GameInstance.GameService.ClanJoinRequestDelete(data.Id, ClanJoinRequestDeleteSuccess, ClanJoinRequestDeleteFail);
+    }
+
+    private void ClanJoinRequestDeleteSuccess(GameServiceResult result)
+    {
+        if (uiPlayerList != null)
+            uiPlayerList.RemoveListItem(data.Id);
+        if (eventClanJoinRequestDeleteSuccess != null)
+            eventClanJoinRequestDeleteSuccess.Invoke();
+    }
+
+    private void ClanJoinRequestDeleteFail(string error)
+    {
+        GameInstance.Singleton.OnGameServiceError(error);
+        if (eventClanJoinRequestDeleteFail != null)
+            eventClanJoinRequestDeleteFail.Invoke();
     }
 }
