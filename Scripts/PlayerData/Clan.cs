175c21da6af6284710e72b19865104c4c54a0855
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2020-03-27 05:20:36 +0700

    Add ownerId, description, owner to clan data

--- Scripts/PlayerData/Clan.cs
+++ Scripts/PlayerData/Clan.cs
@@ -6,8 +6,14 @@ public partial class Clan : IClan
 {
     public string id;
     public string Id { get { return id; } set { id = value; } }
+    public string ownerId;
+    public string OwnerId { get { return ownerId; } set { ownerId = value; } }
     public string name;
     public string Name { get { return name; } set { name = value; } }
+    public string description;
+    public string Description { get { return description; } set { description = value; } }
+    public IPlayer owner;
+    public IPlayer Owner { get { return owner; } set { owner = value; } }
 
     public Clan Clone()
     {
@@ -17,7 +23,10 @@ public partial class Clan : IClan
     public static T CloneTo<T>(IClan from, T to) where T : IClan
     {
         to.Id = from.Id;
+        to.OwnerId = from.OwnerId;
         to.Name = from.Name;
+        to.Description = from.Description;
+        to.Owner = from.Owner;
         return to;
     }
 }
--- Scripts/PlayerData/Interface/IClan.cs
+++ Scripts/PlayerData/Interface/IClan.cs
@@ -1,5 +1,8 @@
 ﻿public partial interface IClan
 {
     string Id { get; set; }
+    string OwnerId { get; set; }
     string Name { get; set; }
+    string Description { get; set; }
+    IPlayer Owner { get; set; }
 }
