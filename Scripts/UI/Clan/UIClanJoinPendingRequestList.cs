db00bee8175fe81175a7d10cadcec3f282a43ec1
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2020-03-27 06:27:11 +0700

    WIP clan UIs

--- Scripts/UI/Clan/UIClanJoinPendingRequestList.cs
+++ Scripts/UI/Clan/UIClanJoinPendingRequestList.cs
@@ -4,5 +4,23 @@ using UnityEngine;
 
 public class UIClanJoinPendingRequestList : UIClanList
 {
+    private void OnEnable()
+    {
+        RefreshList();
+    }
 
+    public void RefreshList()
+    {
+        GameInstance.GameService.GetClanJoinPendingRequestList(OnRefreshListSuccess, OnRefreshListFail);
+    }
+
+    private void OnRefreshListSuccess(ClanListResult result)
+    {
+        SetListItems(result.list);
+    }
+
+    private void OnRefreshListFail(string error)
+    {
+        GameInstance.Singleton.OnGameServiceError(error);
+    }
 }
