414c1296e8a1f0d99ffb0a953119d9f8202ec5e5
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2020-03-27 21:28:59 +0700

    Remove owner id from clan

--- Scripts/PlayerData/Clan.cs
+++ Scripts/PlayerData/Clan.cs
@@ -6,8 +6,6 @@ public partial class Clan : IClan
 {
     public string id;
     public string Id { get { return id; } set { id = value; } }
-    public string ownerId;
-    public string OwnerId { get { return ownerId; } set { ownerId = value; } }
     public string name;
     public string Name { get { return name; } set { name = value; } }
     public string description;
@@ -23,7 +21,6 @@ public partial class Clan : IClan
     public static T CloneTo<T>(IClan from, T to) where T : IClan
     {
         to.Id = from.Id;
-        to.OwnerId = from.OwnerId;
         to.Name = from.Name;
         to.Description = from.Description;
         to.Owner = from.Owner;
--- Scripts/PlayerData/Interface/IClan.cs
+++ Scripts/PlayerData/Interface/IClan.cs
@@ -1,7 +1,6 @@
 ﻿public partial interface IClan
 {
     string Id { get; set; }
-    string OwnerId { get; set; }
     string Name { get; set; }
     string Description { get; set; }
     Player Owner { get; set; }
