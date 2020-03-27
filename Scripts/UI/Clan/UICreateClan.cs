37a9aa9d7e4d257f3505f735459f0ff3ca6f3a6c
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2020-03-27 07:29:42 +0700

    Add UICreateClan

---
+++ Scripts/UI/Clan/UICreateClan.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class UICreateClan : UIBase
+{
+    public InputField inputClanName;
+    public UICurrency uiRequireCurrency;
+    public UIClanManager manager;
+
+    private void OnEnable()
+    {
+        inputClanName.text = "";
+    }
+
+    public void OnClickCreate()
+    {
+        GameInstance.GameService.CreateClan(inputClanName.text, OnCreateClanSuccess, OnCreateClanFail);
+    }
+
+    private void OnCreateClanSuccess(CreateClanResult result)
+    {
+        manager.RefreshData();
+    }
+
+    private void OnCreateClanFail(string error)
+    {
+        GameInstance.Singleton.OnGameServiceError(error);
+    }
+}
