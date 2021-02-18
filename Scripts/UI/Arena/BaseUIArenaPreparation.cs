b81280bce0b3daef0af7d1ff61e4310f40ae3d53
Insthync <ittipon.bay@gmail.com>
2021-02-18 16:33:48 +0700

    Delete unused namespace

--- Scripts/UI/Arena/BaseUIArenaPreparation.cs
+++ Scripts/UI/Arena/BaseUIArenaPreparation.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Events;
-using UnityEngine.UI;
+﻿using UnityEngine.Events;
 
 public abstract class BaseUIArenaPreparation : UIBase
 {
