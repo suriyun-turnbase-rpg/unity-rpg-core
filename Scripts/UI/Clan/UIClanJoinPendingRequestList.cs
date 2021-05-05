b8df008e9ef7f859f40e456a3e400d0d3f2e541c
Insthync <ittipon.bay@gmail.com>
2021-05-06 04:56:05 +0700

    Delete unused namespace

--- Scripts/UI/Clan/UIClanJoinPendingRequestList.cs
+++ Scripts/UI/Clan/UIClanJoinPendingRequestList.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class UIClanJoinPendingRequestList : UIClanList
+﻿public class UIClanJoinPendingRequestList : UIClanList
 {
     private void OnEnable()
     {
--- Scripts/UI/Clan/UIClanJoinRequestList.cs
+++ Scripts/UI/Clan/UIClanJoinRequestList.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class UIClanJoinRequestList : UIPlayerList
+﻿public class UIClanJoinRequestList : UIPlayerList
 {
     public UIPlayer ownerPrefab;
     public UIPlayer managerPrefab;
