714989c5d1756704b8b20b145cef5cbbb5ae574a
Insthync <ittipon.bay@gmail.com>
2021-07-06 02:46:46 +0700

    Add random store event which is generated random store for specific player

---
+++ Scripts/PlayerData/Interface/IRandomStoreEvent.cs
@@ -0,0 +1,6 @@
+﻿public partial interface IRandomStoreEvent
+{
+    string DataId { get; set; }
+    string RandomedItems { get; set; }
+    string PurchaseItems { get; set; }
+}
---
+++ Scripts/PlayerData/RandomStoreEvent.cs
@@ -0,0 +1,38 @@
+﻿[System.Serializable]
+public partial class RandomStoreEvent : IRandomStoreEvent
+{
+    public string dataId;
+    public string DataId { get { return dataId; } set { dataId = value; } }
+    public string randomedItems;
+    public string RandomedItems { get { return randomedItems; } set { randomedItems = value; } }
+    public string purchaseItems;
+    public string PurchaseItems { get { return purchaseItems; } set { purchaseItems = value; } }
+
+    public static GameDatabase GameDatabase
+    {
+        get { return GameInstance.GameDatabase; }
+    }
+
+    public RandomStore RandomStore
+    {
+        get
+        {
+            if (GameDatabase != null && GameDatabase.RandomStores.ContainsKey(DataId))
+                return GameDatabase.RandomStores[DataId];
+            return null;
+        }
+    }
+
+    public RandomStoreEvent Clone()
+    {
+        return CloneTo(this, new RandomStoreEvent());
+    }
+
+    public static T CloneTo<T>(IRandomStoreEvent from, T to) where T : IRandomStoreEvent
+    {
+        to.DataId = from.DataId;
+        to.RandomedItems = from.RandomedItems;
+        to.PurchaseItems = from.PurchaseItems;
+        return to;
+    }
+}
