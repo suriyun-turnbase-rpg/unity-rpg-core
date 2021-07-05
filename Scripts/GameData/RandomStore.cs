bc01f309f6205c555e2878cf282851dea91369b4
Insthync <ittipon.bay@gmail.com>
2021-07-05 20:45:37 +0700

    WIP random store

---
+++ Scripts/GameData/RandomStore.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct RandomStoreItem
+{
+    public ItemAmount rewardItem;
+    public string requireCurrencyId;
+    public int requireCurrencyAmount;
+    public int randomWeight;
+
+    public string Id
+    {
+        get { return rewardItem == null ? "" : rewardItem.Id; }
+    }
+
+    public int Amount
+    {
+        get { return rewardItem == null ? 0 : rewardItem.amount; }
+    }
+
+    public string ToJson()
+    {
+        if (string.IsNullOrEmpty(Id) || Amount <= 0)
+            return "";
+        return "{\"id\":\"" + Id + "\"," +
+            "\"amount\":" + Amount + "," +
+            "\"requireCurrencyId\":" + requireCurrencyId + "," +
+            "\"requireCurrencyAmount\":" + requireCurrencyAmount + "," +
+            "\"randomWeight\":" + randomWeight + "}";
+    }
+}
+
+public class RandomStore : BaseGameData
+{
+    public RandomStoreItem[] items;
+
+    public RandomStoreItem RandomItem()
+    {
+        var weight = new Dictionary<RandomStoreItem, int>();
+        foreach (var item in items)
+        {
+            weight.Add(item, item.randomWeight);
+        }
+        return WeightedRandomizer.From(weight).TakeOne();
+    }
+
+    public virtual string ToJson()
+    {
+        // Random store items
+        var jsonItems = "";
+        foreach (var entry in items)
+        {
+            if (!string.IsNullOrEmpty(jsonItems))
+                jsonItems += ",";
+            jsonItems += entry.ToJson();
+        }
+        jsonItems = "[" + jsonItems + "]";
+        // Combine
+        return "{\"id\":\"" + Id + "\"," +
+            "\"items\":" + jsonItems + "}";
+    }
+}
