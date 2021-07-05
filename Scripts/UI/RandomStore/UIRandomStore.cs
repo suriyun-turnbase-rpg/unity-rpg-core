88e79c54dbb90a60ac1888966837cd744c495c1a
Insthync <ittipon.bay@gmail.com>
2021-07-06 06:00:09 +0700

    Implement random store UIs

---
+++ Scripts/UI/RandomStore/UIRandomStore.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIRandomStore : UIDataItemList<UIRandomStoreItem, RandomStoreItem>
+{
+    public Text textEndsIn;
+    public RandomStore randomStore;
+    public AnimItemsRewarding animItemsRewarding;
+    public RandomStoreEvent StoreEvent { get; private set; }
+    private float refreshCountDown;
+
+    private void OnEnable()
+    {
+        GetRandomStore();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+        if (refreshCountDown > 0)
+            refreshCountDown -= Time.unscaledDeltaTime;
+        if (refreshCountDown < 0)
+            refreshCountDown = 0;
+        if (textEndsIn != null)
+        {
+            textEndsIn.text = refreshCountDown.ToString("N0");
+        }
+    }
+
+    private void GetRandomStore()
+    {
+        ClearListItems();
+        GameInstance.GameService.GetRandomStore(randomStore.Id, OnGetRandomStoreSuccess, OnGetRandomStoreFail);
+    }
+
+    private void OnGetRandomStoreSuccess(RandomStoreResult result)
+    {
+        StoreEvent = result.store;
+        refreshCountDown = result.endsIn;
+        for (var i = 0; i < result.store.RandomedItems.Count; ++i)
+        {
+            var entry = result.store.RandomedItems[i];
+            var ui = SetListItem(i.ToString());
+            ui.data = entry;
+            ui.uiRandomStore = this;
+            ui.index = i;
+            ui.Show();
+        }
+    }
+
+    private void OnGetRandomStoreFail(string error)
+    {
+        GameInstance.Singleton.OnGameServiceError(error, GetRandomStore);
+    }
+}
