31cb6984a5f10e8b68a5db0ae081ef53117c8e4b
Insthync <ittipon.bay@gmail.com>
2021-10-02 14:29:52 +0700

    Delete unused namespace

--- Scripts/GamePlay/BaseCharacterSkill.cs
+++ Scripts/GamePlay/BaseCharacterSkill.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class BaseCharacterSkill
+﻿public abstract class BaseCharacterSkill
 {
     public int Level { get; protected set; }
     public BaseSkill Skill { get; protected set; }
