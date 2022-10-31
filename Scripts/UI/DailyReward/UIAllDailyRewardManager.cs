e4824a1d4760cf4d0344a9df57c3c634f63c060c
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2022-11-01 04:15:18 +0700

    Add UIAllDailyRewardManager

---
+++ Scripts/UI/DailyReward/UIAllDailyRewardManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIAllDailyRewardManager : MonoBehaviour
+{
+    public UIDailyRewardManager[] uiDailyRewardManagers = new UIDailyRewardManager[0];
+
+    public readonly Dictionary<string, UIDailyRewardManager> UIDailyRewardManagers = new Dictionary<string, UIDailyRewardManager>();
+    private void Awake()
+    {
+        foreach (var uiDailyRewardManager in uiDailyRewardManagers)
+        {
+            UIDailyRewardManagers[uiDailyRewardManager.id] = uiDailyRewardManager;
+        }
+    }
+
+    private void Start()
+    {
+        GameInstance.GameService.GetAllDailyRewardList((result) =>
+        {
+            foreach (var dailyReward in result.dailyRewards)
+            {
+                if (UIDailyRewardManagers.TryGetValue(dailyReward.id, out var manager))
+                {
+                    manager.ReloadList(dailyReward.rewards);
+                    manager.Show();
+                }
+            }
+        });
+    }
+}
