6d41dc1ea95456bf8836598b17aa2bcaf7d5b907
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2022-12-23 13:56:41 +0700

    improve: delete unused codes

--- Scripts/UI/Unlockable/UIUnlockItemManager.cs
+++ Scripts/UI/Unlockable/UIUnlockItemManager.cs
@@ -12,53 +12,51 @@ public class UIUnlockItemManager : UIBase
     {
         base.Show();
 
-        if (uiUnlockItemList != null)
+        uiUnlockItemList.selectionMode = UIDataItemSelectionMode.Toggle;
+        uiUnlockItemList.eventSelect.RemoveListener(SelectItem);
+        uiUnlockItemList.eventSelect.AddListener(SelectItem);
+        uiUnlockItemList.eventDeselect.RemoveListener(DeselectItem);
+        uiUnlockItemList.eventDeselect.AddListener(DeselectItem);
+        uiUnlockItemList.ClearListItems();
+
+        var list = new List<string>();
+        var listCharacter = new List<string>();
+        var listEquipment = new List<string>();
+        foreach (var item in GameInstance.GameDatabase.items)
         {
-            uiUnlockItemList.selectionMode = UIDataItemSelectionMode.Toggle;
-            uiUnlockItemList.eventSelect.RemoveListener(SelectItem);
-            uiUnlockItemList.eventSelect.AddListener(SelectItem);
-            uiUnlockItemList.eventDeselect.RemoveListener(DeselectItem);
-            uiUnlockItemList.eventDeselect.AddListener(DeselectItem);
-            uiUnlockItemList.ClearListItems();
+            if (item is CharacterItem)
+                listCharacter.Add(item.Id);
 
-            var list = new List<string>();
-            var listCharacter = new List<string>();
-            var listEquipment = new List<string>();
-            foreach (var item in GameInstance.GameDatabase.items)
+            if (item is EquipmentItem)
+                listEquipment.Add(item.Id);
+        }
+
+        if (showCharacter)
+            list.AddRange(listCharacter);
+
+        if (showEquipment)
+            list.AddRange(listEquipment);
+
+        uiUnlockItemList.SetListItems(list);
+
+        if (dontSelectFirstEntryOnShow)
+        {
+            uiUnlockItemList.DeselectedItems(null);
+        }
+        else
+        {
+            if (uiUnlockItemList.UIEntries.Count > 0)
             {
-                if (item is CharacterItem)
-                    listCharacter.Add(item.Id);
-
-                if (item is EquipmentItem)
-                    listEquipment.Add(item.Id);
-            }
-
-            if (showCharacter)
-                list.AddRange(listCharacter);
-
-            if (showEquipment)
-                list.AddRange(listEquipment);
-            uiUnlockItemList.SetListItems(list);
-
-            if (dontSelectFirstEntryOnShow)
-            {
-                uiUnlockItemList.DeselectedItems(null);
+                var allUIs = uiUnlockItemList.UIEntries.Values.ToList();
+                allUIs[0].Selected = true;
+                SelectItem(allUIs[0]);
             }
             else
             {
-                if (uiUnlockItemList.UIEntries.Count > 0)
+                if (uiSelectedInfo != null)
                 {
-                    var allUIs = uiUnlockItemList.UIEntries.Values.ToList();
-                    allUIs[0].Selected = true;
-                    SelectItem(allUIs[0]);
-                }
-                else
-                {
-                    if (uiSelectedInfo != null)
-                    {
-                        uiSelectedInfo.Clear();
-                        uiSelectedInfo.Hide();
-                    }
+                    uiSelectedInfo.Clear();
+                    uiSelectedInfo.Hide();
                 }
             }
         }
