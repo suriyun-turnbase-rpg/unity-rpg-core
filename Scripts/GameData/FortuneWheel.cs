c2229e1d75ae9233eec1b2840db69a8bfba7b1e7
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2022-12-24 13:14:57 +0700

    feat: WIP fortune wheel system

---
+++ Scripts/GameData/FortuneWheel.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct FortuneWheelReward
+{
+    public ItemAmount[] rewardItems;
+    public int rewardSoftCurrency;
+    public int rewardHardCurrency;
+
+    public string ToJson()
+    {
+        // Reward Items
+        var jsonRewardItems = "";
+        if (rewardItems != null)
+        {
+            foreach (var entry in rewardItems)
+            {
+                if (!string.IsNullOrEmpty(jsonRewardItems))
+                    jsonRewardItems += ",";
+                jsonRewardItems += entry.ToJson();
+            }
+        }
+        jsonRewardItems = "[" + jsonRewardItems + "]";
+        return "{\"rewardSoftCurrency\":" + rewardSoftCurrency + "," +
+            "\"rewardHardCurrency\":" + rewardHardCurrency + "," +
+            "\"rewardItems\":" + jsonRewardItems + "}";
+    }
+}
+
+public enum FortuneWheelRequirementType : byte
+{
+    RequireSoftCurrency = 0,
+    RequireHardCurrency = 1,
+}
+
+public class FortuneWheel : BaseGameData
+{
+    public FortuneWheelRequirementType requirementType;
+    public int price = 0;
+    public FortuneWheelReward[] rewards;
+
+    public virtual string ToJson()
+    {
+        // Reward Items
+        var jsonRewards = "";
+        if (rewards != null)
+        {
+            foreach (var entry in rewards)
+            {
+                if (!string.IsNullOrEmpty(jsonRewards))
+                    jsonRewards += ",";
+                jsonRewards += entry.ToJson();
+            }
+        }
+        jsonRewards = "[" + jsonRewards + "]";
+        return "{\"id\":\"" + Id + "\"," +
+            "\"requirementType\":" + (byte)requirementType + "," +
+            "\"price\":" + price + "," +
+            "\"rewardItems\":" + jsonRewards + "}";
+    }
+}
