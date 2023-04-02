3c2aaa98a5a554df01f2d460a7f63ecd87dfc232
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2023-04-03 05:50:10 +0700

    feat: WIP formation priority

--- Scripts/GamePlay/BaseGamePlayFormation.cs
+++ Scripts/GamePlay/BaseGamePlayFormation.cs
@@ -1,14 +1,70 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class BaseGamePlayFormation : MonoBehaviour
 {
+    [System.Serializable]
+    public struct ContainerData
+    {
+        [Tooltip("Container which character will be stayed")]
+        public Transform container;
+        [Tooltip("Position priority, higher is more far, if character attack mode is attacking by priority it will attack near character first (low priority)")]
+        public int priority;
+    }
+
     public BaseGamePlayManager Manager { get { return BaseGamePlayManager.Singleton; } }
+    // Deprecating
+    [HideInInspector]
     public Transform[] containers;
+    [HideInInspector]
     public Transform helperContainer;
+
+    public List<ContainerData> characterContainers = new List<ContainerData>();
+    public ContainerData helperCharacterContainer = default;
+
     public UIFormationCharacters uiFormationCharacters;
     public readonly Dictionary<int, BaseCharacterEntity> Characters = new Dictionary<int, BaseCharacterEntity>();
+
+    private void Awake()
+    {
+        Migrate();
+    }
+
+    private void OnValidate()
+    {
+        Migrate();
+    }
+
+    private void Migrate()
+    {
+        if (containers != null && containers.Length > 0)
+        {
+            for (int i = 0; i < containers.Length; ++i)
+            {
+                if (i < characterContainers.Count)
+                {
+                    var tempContainer = characterContainers[i];
+                    tempContainer.container = containers[i];
+                    characterContainers[i] = tempContainer;
+                }
+                else
+                {
+                    var tempContainer = new ContainerData()
+                    {
+                        container = containers[i],
+                    };
+                    characterContainers.Add(tempContainer);
+                }
+            }
+        }
+
+        if (helperContainer != null)
+        {
+            var tempContainer = helperCharacterContainer;
+            tempContainer.container = helperContainer;
+            helperCharacterContainer = tempContainer;
+        }
+    }
 
     public virtual void SetFormationCharacters(EBattleType battleType)
     {
